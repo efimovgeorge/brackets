b0ba3dd5ad3036beae67e23be10ed331c02762e9
efimovgeorge <100308528+efimovgeorge@users.noreply.github.com>
2022-08-11 15:58:31 +0300

    init: start brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let open = [];
+    let close = [];
+    let stack = [str[0]];
+    str = str.slice(1, str.length)
+
+    for (i in bracketsConfig) {
+        open.push(bracketsConfig[i][0]);
+        close.push(bracketsConfig[i][1]);
+    }
+
+    for (j in str) {
+        if (open.indexOf(stack[stack.length-1]) !== -1 && open.indexOf(stack[stack.length-1]) === close.indexOf(str[j]) && stack.length > 0) {
+            stack.pop();
+        } else {
+            stack.push(str[j]);
+        }
+    }
+    return stack.length === 0;
 }
